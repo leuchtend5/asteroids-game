1ad27c7a02eb922974070021b83ae34cc4050c87
Gilbert Tan <gilbert.tan@satumedis.id>
2025-03-02 16:08:13 +0700

    feat(asteroid): add asteroid

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -12,9 +14,13 @@ def main():
     y = SCREEN_HEIGHT / 2
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player(x, y, PLAYER_RADIUS)
+    asteroid_field = AsteroidField()
 
 
     while True:
