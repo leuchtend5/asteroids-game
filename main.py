8dea5c868ded75a1e53f8d3baf1f31fa681dc5f7
Gilbert Tan <gilbert.tan@satumedis.id>
2025-03-02 15:52:04 +0700

    feat(player, shape): add player and shape

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(x, y, PLAYER_RADIUS)
+
 
     while True:
         for event in pygame.event.get():
@@ -11,7 +23,14 @@ def main():
                 return
 
         screen.fill("black")
+        updatable.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
